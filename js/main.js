beea3ba413e7c7c49112b713938cf8ad25ff7f83
Jaime <jaime.joseph1991@gmail.com>
2022-04-16 18:26:25 -0400

    added functionality to show pokemon stats

--- js/main.js
+++ js/main.js
@@ -1,24 +1,49 @@
 document.querySelector('button').addEventListener('click',getPokemon)
-
+const stats = document.querySelector('h5')
 
 
 
 
 
 function getPokemon(){
-const choice = document.querySelector('input').value
+const choice = document.querySelector('input').value.toLowerCase()
 
 
 fetch(`https://pokeapi.co/api/v2/pokemon/${choice}`)
     .then(res => res.json())
     .then(data => {
-        document.querySelector('h6').innerHTML = data.name
+        document.querySelector('h6').innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1)
         document.querySelector('img').src = data.sprites.other['official-artwork'].front_default
-        document.querySelector('.pokeStats').innerHTML = `Weight: ${data.weight}`
-        console.log(data.name,data.abilities[0].ability.name)
+        
+           const list = document.querySelector('ul')
+        //append abilites to a list
+         while (list.hasChildNodes()) {
+            list.removeChild(list.firstChild);
+          }
+       data.abilities.forEach(obj => {
+        const li = document.createElement('li')
+           li.innerHTML = obj.ability.name
+           list.appendChild(li)
+         
+           console.log(obj.ability.name)
+       })
+        stats.innerHTML =`Weight: ${data.weight}`
+        const typeListParent = document.querySelector('.type')
+        while (typeListParent.hasChildNodes()){
+            typeListParent.removeChild(typeListParent.firstChild)
+        }
+       data.types.forEach(obj =>{
+           const typeList = document.createElement('li')
+           
+           
+           typeList.textContent = obj.type.name
+           typeListParent.appendChild(typeList)
+          
+       })
+       
     })
     .catch(err => {
-        console.log(`error ${err}`)
+        console.log(`Pokemon not Found ${err}`)
     })
 
 }
